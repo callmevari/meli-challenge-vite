0433e6de01a861af37eeb1e614d63066fa7bd350
callmevari <molinadiego.95@gmail.com>
2024-03-24 20:04:28 -0300

    Implementing SSR through Vite and Express

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+ 
+const App = ({ data }) => {
+  const [count, setCount] = useState(0);
+ 
+  return (
+    <main>
+      <h1>App</h1>
+      <p>Lorem Ipsum</p>
+      <div>
+        <div>{count}</div>
+        <button onClick={() => setCount(count + 1)}>Count</button>
+      </div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </main>
+  );
+};
+ 
+export default App
